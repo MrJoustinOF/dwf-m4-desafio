0e22ea15077f1bd1f2a5965d2420a04fb6d019af
Joustin Ortiz <ortizjoustin@gmail.com>
2021-06-12 14:12:53 -0500

    Cambios en navbar

--- js/navbar.js
+++ js/navbar.js
@@ -5,9 +5,9 @@ const navbar = (el) => {
   <h1><a href="/index.html" class="navbar-link">Joustin</a></h1>
 
   <ul class="navbar-ul">
-    <li class="navbar-ul-li"><a href="/portfolio.html" class="navbar-link">Portfolio</a></li>
-    <li class="navbar-ul-li"><a href="/servicios.html" class="navbar-link">Servicios</a></li>
-    <li class="navbar-ul-li"><a href="/contacto.html" class="navbar-link">Contacto</a></li>
+    <li class="navbar-ul-li"><a href="../portfolio.html" class="navbar-link">Portfolio</a></li>
+    <li class="navbar-ul-li"><a href="../servicios.html" class="navbar-link">Servicios</a></li>
+    <li class="navbar-ul-li"><a href="../contacto.html" class="navbar-link">Contacto</a></li>
   </ul>
 
   <img src="img/btn-menu.png" alt="" class="navbar-btn" />
@@ -16,9 +16,9 @@ const navbar = (el) => {
 <div class="navbar--responsive">
 <img src="img/btn-x.png" alt="" class="navbar-btn--responsive" />
   <ul class="navbar-ul--responsive">
-    <li class="navbar-ul-li--responsive"><a href="/portfolio.html" class="navbar-link">Portfolio</a></li>
-    <li class="navbar-ul-li--responsive"><a href="/servicios.html" class="navbar-link">Servicios</a></li>
-    <li class="navbar-ul-li--responsive"><a href="/contacto.html" class="navbar-link">Contacto</a></li>
+    <li class="navbar-ul-li--responsive"><a href="../portfolio.html" class="navbar-link">Portfolio</a></li>
+    <li class="navbar-ul-li--responsive"><a href="../servicios.html" class="navbar-link">Servicios</a></li>
+    <li class="navbar-ul-li--responsive"><a href="../contacto.html" class="navbar-link">Contacto</a></li>
   </ul>
   
 </div>`;
